80dbb375327ef034ea94bf270f7ea59c01f367cd
suhanjer <suhanjer@gmail.com>
2020-09-15 20:24:57 +0600

    first commit for lecture3 right before django migrate

---
+++ newyear/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+import datetime
+
+# Create your views here.
+now = datetime.datetime.now()
+def index(request):
+    return render(request, "newyear/index.html", {
+        "newyear": now.month == 1 and now.day == 1
+    })
